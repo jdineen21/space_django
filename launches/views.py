a92fc6deb7540d89cfd20a1f1be41fbe2f73f105
Joe Dineen <jdineen21@gmail.com>
2019-10-16 21:19:47 +0100

    added frontend files

--- launches/views.py
+++ launches/views.py
@@ -11,4 +11,6 @@ def index(request):
 def upcoming(request):
     url = 'https://api.spacexdata.com/v3/launches/upcoming'
     r = requests.get(url)
-    return JsonResponse(r.json(), safe=False)
+    #return JsonResponse(r.json(), safe=False)
+    context = {'upcoming_launches': r.json()}
+    return render(request, 'launches/upcoming.html', context)
