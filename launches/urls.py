c84b80521d33e4d27ef71329636c3ffa3923ac3a
Joe Dineen <jdineen21@gmail.com>
2019-10-17 02:05:11 +0100

    details page added for launches

--- launches/urls.py
+++ launches/urls.py
@@ -6,6 +6,8 @@ app_name = 'launches'
 urlpatterns = [
     # ex: launches/
     path('', views.index, name='index'),
+    # ex: launches/5/
+    path('<int:flight_number>/', views.detail, name='detail'),
     # ex: launches/upcoming
     path('upcoming/', views.upcoming, name='upcoming'),
 ]
--- launches/views.py
+++ launches/views.py
@@ -8,6 +8,13 @@ from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 def index(request):
     return render(request, 'launches/index.html')
 
+def detail(request, flight_number):
+    url = 'https://api.spacexdata.com/v3/launches/%s' % (flight_number)
+    r = requests.get(url)
+    #return JsonResponse(r.json(), safe=False)
+    context = {'launch': r.json()}
+    return render(request, 'launches/detail.html', context)
+
 def upcoming(request):
     url = 'https://api.spacexdata.com/v3/launches/upcoming'
     r = requests.get(url)
