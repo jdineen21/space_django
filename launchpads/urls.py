1056b10e443a5136a2530963bb88f0c51cf7292e
Joe Dineen <jdineen21@gmail.com>
2019-10-17 18:41:49 +0100

    added launchpads page

--- launchpads/urls.py
+++ launchpads/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'launchpads'
 urlpatterns = [
     # ex: launchpads/
-    #path('', views.index, name='index'),
+    path('', views.index, name='index'),
     # ex: launchpads/5/
     path('<str:site_id>/', views.detail, name='detail'),
 ]
--- launchpads/views.py
+++ launchpads/views.py
@@ -4,6 +4,13 @@ import json
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 
+def index(request):
+    url = 'https://api.spacexdata.com/v3/launchpads'
+    r = requests.get(url)
+    #return JsonResponse(r.json(), safe=False)
+    context = {'launchpads': r.json()}
+    return render(request, 'launchpads/index.html', context)
+
 def detail(request, site_id):
     url = 'https://api.spacexdata.com/v3/launchpads/%s' % (site_id)
     r = requests.get(url)
