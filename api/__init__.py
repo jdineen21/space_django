32166432714d67c76e615e634a91d566cc769390
Joe Dineen <jdineen21@gmail.com>
2019-10-18 06:50:35 +0100

    added api function to api module

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,28 @@
 import requests
 import datetime
 
+### Cores ###
+
+def get_all_cores():
+    url = 'https://api.spacexdata.com/v3/cores/'
+    r = requests.get(url)
+    return r.json()
+
+def get_upcoming_cores():
+    url = 'https://api.spacexdata.com/v3/cores/upcoming/'
+    r = requests.get(url)
+    return r.json()
+
+def get_past_cores():
+    url = 'https://api.spacexdata.com/v3/cores/past/'
+    r = requests.get(url)
+    return r.json()
+
+def get_core_by_core_serial(core_serial):
+    url = 'https://api.spacexdata.com/v3/cores/%s/' % (core_serial)
+    r = requests.get(url)
+    return r.json()
+
 ### Launches ###
 
 def get_all_launches():
