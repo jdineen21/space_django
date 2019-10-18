d7bff56cf29ac456f6fd59fed2f4bc1b39e40006
Joe Dineen <jdineen21@gmail.com>
2019-10-18 05:27:26 +0100

    added upcoming launches to module

--- api/__init__.py
+++ api/__init__.py
@@ -6,8 +6,13 @@ def get_launches():
     r = requests.get(url)
     return r.json()
 
+def get_upcoming_launches():
+    url = 'https://api.spacexdata.com/v3/launches/upcoming/'
+    r = requests.get(url)
+    return r.json()
+
 def get_launch_by_flight_number(flight_number):
-    url = 'https://api.spacexdata.com/v3/launches/%s' % (flight_number)
+    url = 'https://api.spacexdata.com/v3/launches/%s/' % (flight_number)
     r = requests.get(url)
     launch_json = r.json()
     
@@ -16,3 +21,8 @@ def get_launch_by_flight_number(flight_number):
     launch_json['launch_datetime'] = launch_datetime
 
     return launch_json
+
+def get_launchpad_by_site_id(site_id):
+    url = 'https://api.spacexdata.com/v3/launchpads/%s/' % (site_id)
+    r = requests.get(url)
+    return r.json()
--- launches/views.py
+++ launches/views.py
@@ -2,8 +2,7 @@
 import requests
 import json
 import datetime
-
-from space_django.utils import *
+import api
 
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
@@ -12,19 +11,17 @@ def index(request):
     return render(request, 'launches/index.html')
 
 def detail(request, flight_number):
-    launch_json = get_launch_by_flight_number(flight_number)
-
-    launchpad_url = 'https://api.spacexdata.com/v3/launchpads/%s' % (launch_json['launch_site']['site_id'])
-    launchpad_req = requests.get(launchpad_url)
-    launchpad_json = launchpad_req.json()
-
-    #return JsonResponse(r.json(), safe=False)
-    context = {'launch': launch_json, 'launchpad': launchpad_json}
+    launch_json = api.get_launch_by_flight_number(flight_number)
+    launchpad_json = api.get_launchpad_by_site_id(launch_json['launch_site']['site_id'])
+    
+    context = {
+        'launch': launch_json, 
+        'launchpad': launchpad_json,
+    }
     return render(request, 'launches/detail.html', context)
 
 def upcoming(request):
-    url = 'https://api.spacexdata.com/v3/launches/upcoming'
-    r = requests.get(url)
-    #return JsonResponse(r.json(), safe=False)
-    context = {'upcoming_launches': r.json()}
+    context = {
+        'upcoming_launches': api.get_upcoming_launches(),
+    }
     return render(request, 'launches/upcoming.html', context)
