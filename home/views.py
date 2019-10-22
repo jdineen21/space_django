9a118ca7c66dbc840ab6962b3b5c2e18e8da0916
Joe Dineen <jdineen21@gmail.com>
2019-10-22 11:39:59 +0100

    styling and layout of index page

--- home/views.py
+++ home/views.py
@@ -1,4 +1,11 @@
+import launches
+
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'home/index.html')
+    upcoming_launches = launches.get_upcoming_launches()
+
+    context = {
+        'upcoming_launches': upcoming_launches[:5],
+    }
+    return render(request, 'home/index.html', context)
