c75dabecb6831f6040fde01c76979c199901b76c
Joe Dineen <jdineen21@gmail.com>
2019-10-22 02:27:52 +0100

    removed spacex app

--- spacex/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpacexConfig(AppConfig):
-    name = 'spacex'
--- spacex/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'spacex'
-urlpatterns = [
-    # ex: /spacex/
-    path('', views.index, name='index'),
-]
