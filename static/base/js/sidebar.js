38fd7f8755b0f956ac653b44326516e9e1e3e147
Joe Dineen <jdineen21@gmail.com>
2019-10-23 15:41:42 +0100

    changed dropdown to sidebar

--- static/base/js/sidebar.js
+++ static/base/js/sidebar.js
@@ -2,11 +2,11 @@
 hidden = true
 function slideMenu(id) {
     if (hidden) {
-        document.getElementById("dropdown").style.left = "0";
+        document.getElementById("sidebar").style.left = "0";
         // document.getElementById("content").style.paddingLeft = "10em";
         hidden = false;
     } else {
-        document.getElementById("dropdown").style.left = "-10em";
+        document.getElementById("sidebar").style.left = "-10em";
         // document.getElementById("content").style.paddingLeft = "0";
         hidden = true;
     }
