99ddf334dcc756edc51d96cd495e434db233a9f9
Joe Dineen <jdineen21@gmail.com>
2019-10-26 17:12:40 +0100

    added badge to rockets detail

--- rockets/views.py
+++ rockets/views.py
@@ -1,6 +1,9 @@
 import rockets
+import launches
+import json
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     rockets_all = rockets.get_all_rockets()
@@ -12,8 +15,16 @@ def index(request):
 
 def detail(request, rocket_id):
     rocket = rockets.get_rocket_by_rocket_id(rocket_id)
+    launches_all = launches.get_past_launches()
+
+    launches_all_by_rocket_id = []
+    for launch in launches_all:
+        if launch['rocket']['rocket_id'] == rocket_id:
+            launches_all_by_rocket_id.append(launch)
 
     context = {
         'rocket': rocket,
+        'launch': launches_all_by_rocket_id[0],
+        'launches': launches_all_by_rocket_id,
     }
     return render(request, 'rockets/detail.html', context)
