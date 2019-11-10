41a8e6412b3bc110bfb790e4f5963a9639783868
Joe Dineen <jdineen21@gmail.com>
2019-11-10 09:44:25 +0000

    home screen added significant launches

--- home/views.py
+++ home/views.py
@@ -4,11 +4,19 @@ import random
 from django.shortcuts import render
 
 def index(request):
-    upcoming_launches = launches.get_upcoming_launches()
+    sig = [4, 14, 28, 55]
+    next_launch = launches.get_next_launch()
+    past_launches = launches.get_past_launches()
     random_vid = 'static/base/video/orbit_wo_%s.mp4' % (random.randint(0, 12))
 
+    significant_launches = []
+    for launch in past_launches:
+        if launch['flight_number'] in sig:
+            significant_launches.append(launch)
+
     context = {
-        'upcoming_launches': upcoming_launches[:5],
+        'next_launch': next_launch,
+        'significant_launches': significant_launches[:4],
         'random_vid': random_vid,
     }
     return render(request, 'home/index.html', context)
