85ad4a531112f08795b961d99bb63259f7676179
Joe Dineen <jdineen21@gmail.com>
2019-11-10 05:48:34 +0000

    front page multiple video files

--- home/views.py
+++ home/views.py
@@ -1,12 +1,15 @@
 import launches
+import random
 
 from django.shortcuts import render
 
 def index(request):
     upcoming_launches = launches.get_upcoming_launches()
+    random_vid = 'static/base/video/orbit_wo_%s.mp4' % (random.randint(0, 12))
 
     context = {
         'upcoming_launches': upcoming_launches[:5],
+        'random_vid': random_vid,
     }
     return render(request, 'home/index.html', context)
 
