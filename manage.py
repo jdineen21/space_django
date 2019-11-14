495f6b3bc89e50c9692ac595a4badb9ceac1a296
jdineen21 <jdineen21@gmail.com>
2019-11-14 11:25:26 +0000

    changed manage.py to dev env by default

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'space_django.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'space_django.settings.development')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
