f39576e4dd46e36a64a5fb6162457e93326ca336
jdineen21 <jdineen21@gmail.com>
2019-11-15 19:34:48 +0000

    updated production env

--- space_django/settings/production.py
+++ space_django/settings/production.py
@@ -8,6 +8,6 @@ ALLOWED_HOSTS = ['www.spacextrack.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.dirname(os.path.join(BASE_DIR, 'db.sqlite3')),
+        'NAME': os.path.join(BASE_DIR, 'database/db.sqlite3'),
     }
 }
