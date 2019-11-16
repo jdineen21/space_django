7e7c4a66e794cbcd42e8fa6bdbe4aa219272088f
jdineen21 <jdineen21@gmail.com>
2019-11-16 12:03:09 +0000

    changed production env back

--- space_django/settings/production.py
+++ space_django/settings/production.py
@@ -1,13 +1,6 @@
 
 from .base import *
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.spacextrack.com']
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'database/db.sqlite3'),
-    }
-}
