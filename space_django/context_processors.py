2b2f00ba1e41807770db9347531df2182a563cd5
jdineen21 <jdineen21@gmail.com>
2019-11-16 16:25:32 +0000

    improved context processor code

--- space_django/context_processors.py
+++ space_django/context_processors.py
@@ -8,27 +8,13 @@ def categories_processor(request):
     all_landpads = sorted(landpads.get_landpads(), key = lambda i:(i['status'], i['id']))
     all_rockets = rockets.get_all_rockets()
     all_dragons = dragons.get_all_dragons()
-
-    sidebar = {
-        'launchpads': [],
-        'landpads': [],
-        'rockets': [],
-        'dragons': [],
-    }
-
-    for launchpad in all_launchpads:
-        sidebar['launchpads'].append(launchpad)
-    
-    for landpad in all_landpads:
-        sidebar['landpads'].append(landpad)
-
-    for rocket in all_rockets:
-        sidebar['rockets'].append(rocket)
-    
-    for dragon in all_dragons:
-        sidebar['dragons'].append(dragon)
     
     context = {
-        'sidebar': sidebar,
+        'sidebar': {
+            'launchpads': all_launchpads,
+            'landpads': all_landpads,
+            'rockets': all_rockets,
+            'dragons': all_dragons,
+        },
     }
     return context
