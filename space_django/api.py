584cff747124687a42930d05eca6f51206f3e6a8
Joe <jdineen21@gmail.com>
2020-10-24 20:20:30 +0100

    minor improvement to api function

--- space_django/api.py
+++ space_django/api.py
@@ -9,12 +9,12 @@ url_prefix = 'https://api.spacexdata.com/v3/'
 def external_cached(url_affix, cache_time):
     cache_key = url_affix
     data = cache.get(cache_key, 'has expired')
+    url = '%s%s' % (url_prefix, url_affix)
     if data == 'has expired':
-        logging.info('[MISS] Data Request to %s' % cache_key)
-        url = '%s%s' % (url_prefix, url_affix)
+        logging.debug('[MISS] Data Request to %s' % url)
         data = requests.get(url).json()
         cache.set(cache_key, data, cache_time)
     else:
-        logging.info('[HIT] Data Request to %s' % cache_key)
+        logging.debug('[HIT] Data Request to %s' % url)
     
     return data
